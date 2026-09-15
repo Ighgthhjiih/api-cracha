98809d90d3c217c96f25a626bd2d70e03c6423fc
isaque <isaquesevero8305@gmail.com>
2026-09-15 13:46:57 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host="localhost";
-$usuario="root";
-$bd="escola";
-$senha="";
+$host="api-cracha-isaquesevero8305-9ba8.f.aivencloud.com";
+$usuario="avnadmin";
+$bd="defaultdb";
+$senha="AVNS_i-c8eSMt_9aE324-P48";
 
 $conexao=new mysqli($host,$usuario,$senha,$bd);
 
