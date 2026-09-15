9e667e356e81960b1573c7b861cecf2ed1397e50
isaque <isaquesevero8305@gmail.com>
2026-09-15 13:52:51 -0300

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@ $host="api-cracha-isaquesevero8305-9ba8.f.aivencloud.com";
 $usuario="avnadmin";
 $bd="defaultdb";
 $senha="AVNS_i-c8eSMt_9aE324-P48";
-
-$conexao=new mysqli($host,$usuario,$senha,$bd);
+$porta=24413;
+$conexao=new mysqli($host,$usuario,$senha,$bd,$porta);
 
 
 ?>
